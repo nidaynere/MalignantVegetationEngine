9a0f6aef0370926eedf3fed1a3eca0b0ae096646
Eren Aydin <nidaynere@gmail.com>
2025-06-21 17:56:18 +0200

    Contents

--- Shaders/ColorFilter.hlsl
+++ Shaders/ColorFilter.hlsl
@@ -4,13 +4,19 @@ float _globalColorFilterIntensity;
 void colorFilter_half(
     in float4 objectPosition, 
     in float startFromRadius,
+    in float filterRange,
     in float4 color, 
     out float4 colorWithFilter)
 {
-    float height = objectPosition.y;
+    float height = sqrt(objectPosition.y);
     float2 dir = float2(objectPosition.x, objectPosition.z);
     float mul = max(0, length(dir) - startFromRadius);
     float intensitySign = sign(mul);
     
-    colorWithFilter = lerp(color, _globalColorFilterColor, intensitySign * height * _globalColorFilterIntensity);
+    colorWithFilter = lerp
+    (
+            color,
+            _globalColorFilterColor, 
+            min(1, filterRange * intensitySign * height * _globalColorFilterIntensity)
+    );
 }
